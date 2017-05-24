f0ece8be2f1deacc624981d3fbbc7c9e380c40e7
AiFanGuo <921504734@qq.com>
2017-05-24 16:21:39 +0800

    init

---
+++ app/src/main/java/com/example/myfirstprogram/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.myfirstprogram;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myfirstprogram.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        onWindowFocusChanged(true);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showToast(getMobileWidth()+getMobileHeight()+getStatusBarHeight()+"__"+checkNetworkState());
+    }
+
+
+
+    @Override
+    protected void loadViewLayout() {
+
+
+    }
+
+    @Override
+    protected void findViewById() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void processLogic() {
+
+    }
+
+    @Override
+    protected Context getActivityContext() {
+        return null;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
